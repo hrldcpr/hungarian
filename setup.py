227aa0b85472f382ad332b623b8a11fb529daa14
Harold Cooper <hcooper@ebay.com>
2013-01-23 17:38:13 -0500

    need to include header file, 'python setup.py sdist upload' doesn't upload it to pypi

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('', parent_package,top_path)
-    config.add_extension('hungarian', ['asp.cpp','hungarian.cpp'])
+    config.add_extension('hungarian', ['asp.cpp', 'asp.h', 'hungarian.cpp'])
     return config
 
 if __name__ == "__main__":
     from numpy.distutils.core import setup
     setup(configuration=configuration,
           name='hungarian',
-          version='0.2.2',
+          version='0.2.3',
           url='http://github.com/hrldcpr/hungarian',
           description='algorithm for the linear assignment problem',
           long_description=
